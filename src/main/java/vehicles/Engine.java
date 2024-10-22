f4142ab8fee72976bf2ca949cf446f94b3108fb4
yee0802 <NovusAa12@proton.me>
2024-10-22 12:03:30 +0100

    fix: add constructor to Engine class

--- src/main/java/vehicles/Engine.java
+++ src/main/java/vehicles/Engine.java
@@ -4,4 +4,10 @@ public class Engine {
     public boolean running;
     public int horsepower;
     public String fuelType;
+
+    public Engine(boolean running, int horsepower, String fuelType) {
+        this.running = running;
+        this.horsepower = horsepower;
+        this.fuelType = fuelType;
+    }
 }
