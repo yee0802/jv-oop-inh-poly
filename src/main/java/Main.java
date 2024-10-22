41c98fbf41647f597ecc9761f0b12f44ceef5c1c
yee0802 <NovusAa12@proton.me>
2024-10-22 15:48:56 +0100

    feat: add Shape functionality test to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
+import shapes.Circle;
+import shapes.Rectangle;
+import shapes.Shape;
+import shapes.Triangle;
 import vehicles.Car;
 import vehicles.Motorcycle;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        /*
         Car ferrari = new Car("Ferrari", "la-ferrari");
         Car tesla = new Car("Tesla", "Y");
 
@@ -17,5 +24,16 @@ public class Main {
 
         honda.drive();
         germanMotorcycle.drive();
+        */
+
+        List<Shape> shapes = List.of(
+                new Circle(5),
+                new Rectangle(2, 5),
+                new Triangle(3, 7)
+        );
+
+        for (Shape shape : shapes) {
+            System.out.println(shape.getClass().getSimpleName() + " area = " + shape.calculateArea());
+        }
     }
 }
