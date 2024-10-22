f348259bfaabee6bd7a4415a286aa1e4fceb4b5d
yee0802 <NovusAa12@proton.me>
2024-10-22 15:18:37 +0100

    feat: add functionality tests of Motorcycle & Car methods to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import vehicles.Car;
+import vehicles.Motorcycle;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Car ferrari = new Car("Ferrari", "la-ferrari");
+        Car tesla = new Car("Tesla", "Y");
+
+        Motorcycle honda = new Motorcycle("Honda", "PCX", false);
+        Motorcycle germanMotorcycle = new Motorcycle("BMW", "sidecar", true);
+
+        ferrari.drive();
+        tesla.drive();
+
+        honda.start();
+        germanMotorcycle.start();
+
+        honda.drive();
+        germanMotorcycle.drive();
     }
 }
