6e68f2dc0bf6207baea05f9940053182434e2b29
yee0802 <NovusAa12@proton.me>
2024-10-22 15:11:41 +0100

    feat: add accelerate method to Motorcycle

--- src/main/java/vehicles/Motorcycle.java
+++ src/main/java/vehicles/Motorcycle.java
@@ -8,4 +8,18 @@ public class Motorcycle extends Vehicle {
         this.hasSidecar = hasSidecar;
         this.engine = new MotorcycleEngine();
     }
+
+    @Override
+    protected void accelerate() {
+        String message = make + " " + model + " speed: ";
+        double fuelMod = switch (this.engine.fuelType) {
+            case "Unleaded" -> 1.5D;
+            case "Leaded" -> 1.2D;
+            case "Diesel" -> 1.8D;
+            default -> throw new IllegalStateException("Unexpected value: " + this.engine.fuelType);
+        };
+        for (int i = 0 ; i < 10 ; i++) {
+            System.out.println(message + String.format("%.2f", ((engine.horsepower * fuelMod) * Math.exp(Math.sqrt(i)) / 2)* (hasSidecar ? 0.8 : 1.0)));
+        }
+    }
 }
