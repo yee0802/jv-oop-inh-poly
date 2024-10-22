b0f6a9c7fcf656f6b6e003efa9a6703d5c2e09fc
yee0802 <NovusAa12@proton.me>
2024-10-22 12:21:07 +0100

    feat: add constructor to Motorcycle class

--- src/main/java/vehicles/Motorcycle.java
+++ src/main/java/vehicles/Motorcycle.java
@@ -1,5 +1,11 @@
 package vehicles;
 
-public class Motorcycle {
+public class Motorcycle extends Vehicle {
     public boolean hasSidecar;
+
+    public Motorcycle(String make, String model, boolean hasSidecar) {
+        super(make, model);
+        this.hasSidecar = hasSidecar;
+        this.engine = new MotorcycleEngine();
+    }
 }
